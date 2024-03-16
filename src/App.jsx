582190eb6eb088a4f43e0860ef338a96b5bc6661
safin878 <shafin6747@gmail.com>
2024-03-16 09:40:34 +0600

    Our recipes section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
+import OurRecipe from "./components/OurRecipe";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <OurRecipe></OurRecipe>
     </>
   );
 }
---
+++ src/components/OurRecipe.jsx
@@ -0,0 +1,16 @@
+const OurRecipe = () => {
+  return (
+    <div className="container mx-auto text-center my-16">
+      <h2 className="text-4xl font-bold">Our Recipes</h2>
+      <h5 className="lg:px-36 p-3 text-[#150B2B99] font-medium">
+        Dive into Our Recipes, your gateway to culinary excellence. Explore a
+        rich tapestry of flavors and techniques, crafted with passion. Whether
+        you're a beginner or a seasoned chef, our recipes will elevate your
+        kitchen adventures. Let us inspire your palate and ignite your love for
+        cooking.
+      </h5>
+    </div>
+  );
+};
+
+export default OurRecipe;
